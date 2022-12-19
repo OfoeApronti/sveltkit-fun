d488306e75170009975986deacdf11514d3f55cd
ofoe.apronti <ofoe.apronti@gmail.com>
2022-12-19 19:20:37 +0000

    added movie feature

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
-<nav>
-<a href="/">Home</a>
-<a href="/shop" data-sveltekit-prefetch>Shop</a>
-<a href="/movies">Movies</a>
-</nav>
-<slot />
+<script>
+  import "../app.css";
+</script>
+
+<div class="flex min-h-full flex-col py-12 sm:px-6 lg:px-8 w-full">
+  <div class="sm:mx-auto sm:w-full ">
+    <nav class="flex pb-2 space-x-8">
+      <a
+        href="/"
+        class="btn btn-primary bg-gray-300 p-2 rounded hover:bg-gray-400"
+        >Home</a
+      >
+      <a href="/" class="btn bg-gray-300 p-2 rounded hover:bg-gray-400"
+        >Contact</a
+      >
+      <a
+        href="/shop"
+        data-sveltekit-prefetch
+        class="btn bg-gray-300 p-2 rounded hover:bg-gray-400">Shop</a
+      >
+      <a href="/movies" class="btn bg-gray-300 p-2 rounded hover:bg-gray-400"
+        >Movies</a
+      >
+    </nav>
+    <slot />
+  </div>
+</div>
---
+++ src/routes/movies/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let data;
+  let { movies } = data;
+  console.log(data);
+</script>
+
+<h1>Movies</h1>
+
+{#each movies as movie}
+<div class="mt-4">
+  <h2 class="text-xl font-semibold">{movie.title}</h2>
+  <p>{movie.overview}</p>
+</div>
+  
+{/each}
+
