2bc02e5e2253bda67e0dc4ff4ddb2813fc4b029e
ofoe.apronti <ofoe.apronti@gmail.com>
2022-12-26 13:46:20 +0000

    commented out bash commands in groovy script

--- script.groovy
+++ script.groovy
@@ -8,13 +8,13 @@ def deployApp(){
   echo "deploying the application"
   echo "execute version: ${params.VERSION}"
 }
-def updateVersionsh(){
-  echo "reading version"
-  sh "read ver <<< $(awk -F\":\" '/\"version\":.\"[0-9]\.[0-9]\.[0-9]/ {print ${2}}' package.json | sed 's/,////; s/\"////g')"
-  sh "IFS=\".\" read -a version <<< ${ver}"
-  echo "the package version: $version"
+// def updateVersionsh(){
+//   echo "reading version"
+//   sh "read ver <<< $(awk -F\":\" '/\"version\":.\"[0-9]\.[0-9]\.[0-9]/ {print ${2}}' package.json | sed 's/,////; s/\"////g')"
+//   sh "IFS=\".\" read -a version <<< ${ver}"
+//   echo "the package version: $version"
 
-}
+// }
 def updateVersion(){
   fields=readJSON file: 'package.josn'
   versions= fields['version'].split('.')
