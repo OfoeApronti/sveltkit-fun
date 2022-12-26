83b232965be04d84a0945bf3b703654321d1472d
ofoe.apronti <ofoe.apronti@gmail.com>
2022-12-26 13:41:02 +0000

    correcting jenkins errors

--- getversion.sh
+++ getversion.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 read ver <<< $(awk -F":" '/"version":."[0-9]\.[0-9]\.[0-9]/ {print $2;exit}' package.json | sed 's/,//; s/"//g')
 IFS=\".\" read -a version <<< ${ver}
-echo "The package version: $ver"
+echo "the package version: $version"
 major=${version[0]}
 minor=${version[1]}
 patch=${version[2]}
-patch=$(($patch+1))
-export NEW_VERSION="$major"."$minor"."$patch"
-echo "The new package version: $NEW_VERSION"
+patch=$patch+1
+export NEW_VERSION="$major"."$minor"."$"
